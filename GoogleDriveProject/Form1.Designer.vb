20b71f74d0c6f3f8af46cc805ad98f2a6d6c95aa
teekungx <58821364+teekungx@users.noreply.github.com>
2020-01-11 22:30:08 +0700

    Upload File
    Upload File

---
+++ GoogleDriveProject/Form1.Designer.vb
@@ -0,0 +1,160 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Checking = New System.Windows.Forms.Timer(Me.components)
+        Me.GetInfo = New System.Windows.Forms.Timer(Me.components)
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.TextBox8 = New System.Windows.Forms.TextBox()
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.FilePath = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(21, 17)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Browse"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(21, 46)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(345, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Upload on Google Drive"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(21, 174)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(345, 23)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "Copy File"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(21, 148)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(345, 20)
+        Me.TextBox1.TabIndex = 4
+        Me.TextBox1.Text = "File ID"
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Checking
+        '
+        Me.Checking.Interval = 3000
+        '
+        'GetInfo
+        '
+        Me.GetInfo.Interval = 3000
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(21, 84)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(345, 20)
+        Me.TextBox4.TabIndex = 15
+        Me.TextBox4.Text = "File ID"
+        Me.TextBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(21, 110)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(345, 23)
+        Me.Button5.TabIndex = 14
+        Me.Button5.Text = "Delete File"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'TextBox8
+        '
+        Me.TextBox8.Location = New System.Drawing.Point(21, 203)
+        Me.TextBox8.Name = "TextBox8"
+        Me.TextBox8.Size = New System.Drawing.Size(345, 20)
+        Me.TextBox8.TabIndex = 26
+        Me.TextBox8.Text = "File ID"
+        Me.TextBox8.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Button11
+        '
+        Me.Button11.Location = New System.Drawing.Point(21, 229)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(345, 23)
+        Me.Button11.TabIndex = 25
+        Me.Button11.Text = "Show File Owner"
+        Me.Button11.UseVisualStyleBackColor = True
+        '
+        'FilePath
+        '
+        Me.FilePath.Location = New System.Drawing.Point(102, 19)
+        Me.FilePath.Name = "FilePath"
+        Me.FilePath.Size = New System.Drawing.Size(264, 20)
+        Me.FilePath.TabIndex = 27
+        Me.FilePath.Text = "File Path"
+        Me.FilePath.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(385, 272)
+        Me.Controls.Add(Me.FilePath)
+        Me.Controls.Add(Me.TextBox8)
+        Me.Controls.Add(Me.Button11)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Google Drive API Example By Tee"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Checking As Timer
+    Friend WithEvents GetInfo As Timer
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Button5 As Button
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents Button11 As Button
+    Friend WithEvents FilePath As TextBox
+End Class
---
+++ GoogleDriveProject/Form1.vb
@@ -0,0 +1,144 @@
+﻿Imports System.Data.SqlClient
+Imports System.Configuration
+Imports Google.Apis.Auth
+Imports Google.Apis.Download
+Imports Google.Apis.Drive.v2
+Imports Google.Apis.Auth.OAuth2
+Imports Google.Apis.Services
+Imports System.Threading
+Imports Google.Apis.Drive.v2.Data
+Imports System.Windows.Forms.Application
+Imports System.Text.RegularExpressions
+
+Public Class Form1
+    'SYSTEM'
+    Dim service As New DriveService
+
+    Private Sub createservice()
+        Dim clientid = "clientid"
+        Dim clientsecret = "secretid"
+        Dim uc As UserCredential = GoogleWebAuthorizationBroker.AuthorizeAsync(New ClientSecrets() With {.clientid = clientid, .clientsecret = clientsecret}, {DriveService.Scope.Drive}, "user", CancellationToken.None).Result
+        service = New DriveService(New BaseClientService.Initializer() With {.HttpClientInitializer = uc, .ApplicationName = "GoogleDriveProject"})
+
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim f As New OpenFileDialog
+        If f.ShowDialog = DialogResult.OK Then
+            FilePath.Text = f.FileName
+        Else
+            Exit Sub
+        End If
+    End Sub
+
+
+    Public Shared Function CopyFile(ByVal service As DriveService, ByVal originFileId As String, ByVal copyTitle As String) As File
+
+
+        Dim copiedFile As File = New File()
+        copiedFile.Title = printTitle(service, originFileId)
+        copiedFile.MimeType = printMeme(service, originFileId)
+        Try
+            Return service.Files.Copy(copiedFile, originFileId).Execute()
+        Catch e As Exception
+            'MessageBox.Show("An error occurred: " & e.Message)
+        End Try
+        Return Nothing
+    End Function
+
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If service.ApplicationName <> "GoogleDriveProject" Then
+            createservice()
+        End If
+        Dim myfile As New File()
+        Dim bytearray As Byte() = System.IO.File.ReadAllBytes(FilePath.Text)
+        Dim stream As New System.IO.MemoryStream(bytearray)
+        Dim uploadrequest As FilesResource.InsertMediaUpload = service.Files.Insert(myfile, stream, myfile.MimeType)
+        uploadrequest.Upload()
+        Dim file As File = uploadrequest.ResponseBody
+        TextBox1.Text = file.Id
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If service.ApplicationName <> "GoogleDriveProject" Then
+            createservice()
+        End If
+        MessageBox.Show(CopyFile(service, TextBox1.Text, "test").Id)
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs)
+    End Sub
+
+    Public Shared Sub DeleteFile(ByVal service As DriveService, ByVal fileId As String)
+        Try
+            service.Files.Delete(fileId).Execute()
+        Catch e As Exception
+            Console.WriteLine("An error occurred: " & e.Message)
+        End Try
+    End Sub
+    Dim Data As String
+
+
+
+    Public Shared Function printTitle(ByVal service As DriveService, ByVal fileId As String)
+        Try
+            Dim file As File = service.Files.[Get](fileId).Execute()
+            Console.WriteLine("Title: " & file.Title)
+            Console.WriteLine("Description: " & file.Description)
+            Console.WriteLine("MIME type: " & file.MimeType)
+            Return file.Title
+        Catch e As Exception
+            Console.WriteLine("An error occurred: " & e.Message)
+            Return ""
+        End Try
+    End Function
+
+    Public Shared Function printOwner(ByVal service As DriveService, ByVal fileId As String)
+        Try
+            Dim file As File = service.Files.[Get](fileId).Execute()
+            Return file.OwnerNames(0)
+        Catch e As Exception
+            Return ""
+        End Try
+    End Function
+
+
+    Public Shared Function printMeme(ByVal service As DriveService, ByVal fileId As String)
+        Try
+            Dim file As File = service.Files.[Get](fileId).Execute()
+            Console.WriteLine("Title: " & file.Title)
+            Console.WriteLine("Description: " & file.Description)
+            Console.WriteLine("MIME type: " & file.MimeType)
+            Return file.MimeType
+        Catch e As Exception
+            Console.WriteLine("An error occurred: " & e.Message)
+            Return ""
+        End Try
+    End Function
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        If service.ApplicationName <> "GoogleDriveProject" Then
+            createservice()
+        End If
+        DeleteFile(service, TextBox4.Text)
+    End Sub
+
+
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+        If service.ApplicationName <> "GoogleDriveProject" Then
+            createservice()
+        End If
+        Dim title As String = printOwner(service, TextBox8.Text)
+        MessageBox.Show(title)
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+End Class
